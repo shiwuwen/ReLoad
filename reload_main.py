dd5d9067d26e55778830d2eb112d5ad8f4cc5557
wsw <1098243691@qq.com>
2020-07-30 16:36:03 +0800

    20200730changed

--- reload_main.py
+++ reload_main.py
@@ -1,4 +1,81 @@
+import tensorflow as tf 
+import numpy as np
+import time
+
 from reload_environment import Environment
+from reload_rl_DDPG import Actor, Critic, Memory
+
+
+#####################  hyper parameters  ####################
+
+MAX_EPISODES = 1000
+MAX_EP_STEPS = 200
+
+LR_A = 0.001    # learning rate for actor
+LR_C = 0.001    # learning rate for critic
+GAMMA = 0.9     # reward discount
+
+REPLACEMENT = [
+    dict(name='soft', tau=0.01),
+    dict(name='hard', rep_iter_a=600, rep_iter_c=500)
+][0]            # you can try different target replacement strategies
+
+MEMORY_CAPACITY = 10000
+BATCH_SIZE = 16
+
+OUTPUT_GRAPH = True
+
 
 env = Environment()
-print(env.step())
+state_dim = env.state_dim
+action_dim = env.action_dim
+
+with tf.name_scope('S'):
+	S = tf.placeholder(tf.float32, shape=[None, state_dim], name='s')
+with tf.name_scope('R'):
+	R = tf.placeholder(tf.float32, [None, 1], name='r')
+with tf.name_scope('S_'):
+	S_ = tf.placeholder(tf.float32, shape=[None, state_dim], name='s_')
+
+sess = tf.Session()
+
+actor = Actor(sess, action_dim, LR_A, REPLACEMENT, S, R, S_)
+critic = Critic(sess, state_dim, action_dim, LR_C, GAMMA, REPLACEMENT, actor.a, actor.a_, S, R, S_)
+actor.add_grad_to_graph(critic.a_grads)
+
+sess.run(tf.global_variables_initializer())
+
+M = Memory(MEMORY_CAPACITY, dims=2 * state_dim + action_dim + 1)
+
+if OUTPUT_GRAPH:
+	tf.summary.FileWriter("logs/", sess.graph)
+
+t1 = time.time()
+for i in range(MAX_EPISODES):
+	s = env.reset()
+	ep_reward = 0
+
+	for j in range(MAX_EP_STEPS):
+
+		a = actor.choose_action(s)
+		s_, r = env.step(a)
+
+		M.store_transition(s, a, r, s_)
+
+		if M.pointer > MEMORY_CAPACITY:
+			b_M = M.sample(BATCH_SIZE)
+			b_s = b_M[:, :state_dim]
+			b_a = b_M[:, state_dim:state_dim+action_dim]
+			b_r = b_M[:, -state_dim-1: -state_dim]
+			b_s_ = b_M[:, -state_dim:]
+
+			critic.learn(b_s, b_a, b_r, b_s_)
+			actor.learn(b_s)
+
+		s = s_
+		ep_reward += r
+
+		if j == MAX_EP_STEPS-1:
+			print('Episode:', i, ' Reward: %i' % int(ep_reward))
+
+print('Running time: ', time.time()-t1)
--- reload_rl_DDPG.py
+++ reload_rl_DDPG.py
@@ -1,47 +1,29 @@
-
 import tensorflow as tf
 import numpy as np
-import gym
 import time
 
 
 np.random.seed(1)
 tf.set_random_seed(1)
 
-#####################  hyper parameters  ####################
-
-MAX_EPISODES = 200
-MAX_EP_STEPS = 200
-LR_A = 0.001    # learning rate for actor
-LR_C = 0.001    # learning rate for critic
-GAMMA = 0.9     # reward discount
-REPLACEMENT = [
-    dict(name='soft', tau=0.01),
-    dict(name='hard', rep_iter_a=600, rep_iter_c=500)
-][0]            # you can try different target replacement strategies
-MEMORY_CAPACITY = 10000
-BATCH_SIZE = 32
-
-RENDER = False
-OUTPUT_GRAPH = True
-ENV_NAME = 'Pendulum-v0'
-
-
 class Actor(object):
-    def __init__(self, sess, action_dim, action_bound, learning_rate, replacement):
+    def __init__(self, sess, action_dim, learning_rate, replacement, S, R, S_):
         self.sess = sess
         self.a_dim = action_dim
-        self.action_bound = action_bound
+        # self.action_bound = action_bound
         self.lr = learning_rate
         self.replacement = replacement
         self.t_replace_counter = 0
+        self.S = S
+        self.R = R
+        self.S_ = S_
 
         with tf.variable_scope('Actor'):
             # input s, output a
-            self.a = self._build_net(S, scope='eval_net', trainable=True)
+            self.a = self._build_net(self.S, scope='eval_net', trainable=True)
 
             # input s_, output a, get a_ for critic
-            self.a_ = self._build_net(S_, scope='target_net', trainable=False)
+            self.a_ = self._build_net(self.S_, scope='target_net', trainable=False)
 
         self.e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Actor/eval_net')
         self.t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Actor/target_net')
@@ -63,11 +45,11 @@ class Actor(object):
             with tf.variable_scope('a'):
                 actions = tf.layers.dense(net, self.a_dim, activation=tf.nn.tanh, kernel_initializer=init_w,
                                           bias_initializer=init_b, name='a', trainable=trainable)
-                scaled_a = tf.multiply(actions, self.action_bound, name='scaled_a')  # Scale output to -action_bound to action_bound
-        return scaled_a
+                # scaled_a = tf.multiply(actions, self.action_bound, name='scaled_a')  # Scale output to -action_bound to action_bound
+        return actions #scaled_a
 
     def learn(self, s):   # batch update
-        self.sess.run(self.train_op, feed_dict={S: s})
+        self.sess.run(self.train_op, feed_dict={self.S: s})
 
         if self.replacement['name'] == 'soft':
             self.sess.run(self.soft_replace)
@@ -78,7 +60,7 @@ class Actor(object):
 
     def choose_action(self, s):
         s = s[np.newaxis, :]    # single state
-        return self.sess.run(self.a, feed_dict={S: s})[0]  # single action
+        return self.sess.run(self.a, feed_dict={self.S: s})[0]  # single action
 
     def add_grad_to_graph(self, a_grads):
         with tf.variable_scope('policy_grads'):
@@ -96,27 +78,30 @@ class Actor(object):
 ###############################  Critic  ####################################
 
 class Critic(object):
-    def __init__(self, sess, state_dim, action_dim, learning_rate, gamma, replacement, a, a_):
+    def __init__(self, sess, state_dim, action_dim, learning_rate, gamma, replacement, a, a_, S, R, S_):
         self.sess = sess
         self.s_dim = state_dim
         self.a_dim = action_dim
         self.lr = learning_rate
         self.gamma = gamma
         self.replacement = replacement
-
+        self.S = S
+        self.R = R
+        self.S_ = S_
+        
         with tf.variable_scope('Critic'):
             # Input (s, a), output q
             self.a = tf.stop_gradient(a)    # stop critic update flows to actor
-            self.q = self._build_net(S, self.a, 'eval_net', trainable=True)
+            self.q = self._build_net(self.S, self.a, 'eval_net', trainable=True)
 
             # Input (s_, a_), output q_ for q_target
-            self.q_ = self._build_net(S_, a_, 'target_net', trainable=False)    # target_q is based on a_ from Actor's target_net
+            self.q_ = self._build_net(self.S_, a_, 'target_net', trainable=False)    # target_q is based on a_ from Actor's target_net
 
             self.e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/eval_net')
             self.t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Critic/target_net')
 
         with tf.variable_scope('target_q'):
-            self.target_q = R + self.gamma * self.q_
+            self.target_q = self.R + self.gamma * self.q_
 
         with tf.variable_scope('TD_error'):
             self.loss = tf.reduce_mean(tf.squared_difference(self.target_q, self.q))
@@ -151,7 +136,7 @@ class Critic(object):
         return q
 
     def learn(self, s, a, r, s_):
-        self.sess.run(self.train_op, feed_dict={S: s, self.a: a, R: r, S_: s_})
+        self.sess.run(self.train_op, feed_dict={self.S: s, self.a: a, self.R: r, self.S_: s_})
         if self.replacement['name'] == 'soft':
             self.sess.run(self.soft_replacement)
         else:
